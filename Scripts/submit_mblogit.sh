d196576fe02b9b11bbed3b32b7c5afdeef5280e4
Omar Lizardo <omarlizardo@gmail.com>
2026-08-20 14:26:57 -0700

    do it

---
+++ Scripts/submit_mblogit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -cwd
+#$ -j y
+#$ -o mblogit_job.log
+#$ -l h_rt=23:50:00
+#$ -l h_data=16G
+#$ -pe shared 4
+
+module load gcc/10.2.0
+module load R
+
+Rscript Scripts/run_mblogit.R
